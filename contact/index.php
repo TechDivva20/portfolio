2d37d3d0f74841755749dce30de0cbe0abdd629c
Samuel Akins <akinyemisamuel170@gmail.com>
2023-08-23 16:02:20 +0100

    Integrate contact mailing

---
+++ contact/index.php
@@ -0,0 +1,49 @@
+<?php
+
+require '../vendor/PHPMailer-master/src/PHPMailer.php';
+require '../vendor/PHPMailer-master/src/SMTP.php';
+require '../vendor/PHPMailer-master/src/Exception.php';
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+$name = $_POST["name"];
+$from = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+$to = "BlessingPatrick0610@outlook.com";
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+    $mail->isSMTP(); //Send using SMTP
+    $mail->Host = 'twentythree.qservers.net'; //Set the SMTP server to send through
+    $mail->SMTPAuth = true; //Enable SMTP authentication
+    $mail->Username = 'blessing@sannex.ng'; //SMTP username
+    $mail->Password = 'Blessing.sannex'; //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom("$from", "$name");
+    $mail->addAddress("$to", 'Blessing Patrick'); //Add a recipient
+
+    //Content
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = "$subject";
+    $mail->Body = "$message";
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Horray! \n Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
